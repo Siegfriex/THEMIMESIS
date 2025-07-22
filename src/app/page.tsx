8e0d071c3b1569ae6d1eb543ef6d011060e86d0f
Siegfriex <6siegfriex@gmail.com>
2025-07-23 02:15:12 +0900

    Delete src/app directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MimesisApp } from '@/components/mimesis-app';
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen w-full items-center justify-center p-4">
-      <MimesisApp />
-    </div>
-  );
-}
